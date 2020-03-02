77918066667769ac6d461f6c61a2439d114a2d1e
cpcwood <cpcwood@outlook.com>
2020-03-02 15:51:00 +0000

    Background scrolling

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,56 +1,96 @@
-var cvs = document.getElementById('canvas')
+var canvas = document.getElementById('canvas')
+var canvasContext = canvas.getContext('2d')
 
-var ctx = cvs.getContext('2d')
+// Create image objcects
 
-var dino = new Image()
-dino.src = 'images/dino_run.png'
-var background = new Image()
-background.src = 'images/background_desert.png'
+var dinoImage = new Image()
+dinoImage.src = 'images/dino_run.png'
+var backgroundImage = new Image()
+backgroundImage.src = 'images/background_desert.png'
 var stoneBlock = new Image()
 stoneBlock.src = 'images/stone_block.png'
 
-var dinoX = 100
-var dinoY = 100
+class Background {
+  constructor() {
+    this.x = 0
+    this.y = 0
+  }
 
-var blocks = []
+  reset() {
+    this.x = canvas.width
+  }
+
+  move() {
+    this.x -= 2.5
+  }
+}
+var backgroundArray = []
+backgroundArray.push(new Background())
+let secondBackground = new Background()
+secondBackground.reset()
+backgroundArray.push(secondBackground)
+console.log(backgroundArray)
+
+class Dino {
+  constructor() {
+    this.x = 100
+    this.y = 100
+  }
+
+  applyGravity() {
+    this.y += 5
+  }
+}
+var dino = new Dino()
+
 
 class Block {
   constructor() {
-    this.x = cvs.width
-    this.y = cvs.height - 200
+    this.x = canvas.width
+    this.y = canvas.height - 200
   }
 
   move() {
     this.x -= 5
   }
 }
-
+var blocks = []
 blocks.push(new Block())
 
 
 
 function draw() {
+  // Draw background
+  for (var i = 0; i < backgroundArray.length; i++) {
+    canvasContext.drawImage(backgroundImage, backgroundArray[i].x, backgroundArray[i].y)
+    if (backgroundArray[i].x == -canvas.width) {
+      backgroundArray[i].reset()
+    }
+    else {
+      backgroundArray[i].move()
+    }
+  }
+
+
+  // Draw Dino
+  canvasContext.drawImage(dinoImage,dino.x,dino.y,100,100)
+  dino.applyGravity()
+
+
+
   // Draw blocks
-
-
-  ctx.drawImage(background,0,0)
-  ctx.drawImage(dino,dinoX,dinoY,100,100)
-
   for (var i = 0; i < blocks.length; i++) {
-    ctx.drawImage(stoneBlock, blocks[i].x, blocks[i].y, 80, 80)
+    canvasContext.drawImage(stoneBlock, blocks[i].x, blocks[i].y, 80, 80)
     blocks[i].move()
 
-    if ((i == blocks.length - 1) && (blocks[i].x <= cvs.width - 240)) {
+    if ((i == blocks.length - 1) && (blocks[i].x <= canvas.width - 240)) {
       if (Math.random(1) < 0.05) {
         blocks.push(new Block())
       }
     }
-
   }
 
 
-
-  dinoY += 10
   requestAnimationFrame(draw)
 
 }
