7f10c79b732958c02d19c0b1fd122a8596f7b6ab
cpcwood <cpcwood@outlook.com>
2020-03-02 17:56:49 +0000

    limited player to not drop below ground, double jumps configured

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,6 +9,12 @@ var backgroundImage = new Image()
 backgroundImage.src = 'images/background_desert.png'
 var stoneBlock = new Image()
 stoneBlock.src = 'images/stone_block.png'
+var groundCentreImage = new Image()
+var groundLeftImage = new Image()
+var groundRightImage = new Image()
+groundCentreImage.src = 'images/ground_centre.png'
+groundLeftImage.src = 'images/ground_left.png'
+groundRightImage.src = 'images/ground_right.png'
 
 class Background {
   constructor() {
@@ -29,19 +35,62 @@ backgroundArray.push(new Background())
 let secondBackground = new Background()
 secondBackground.reset()
 backgroundArray.push(secondBackground)
-console.log(backgroundArray)
+
+
+
+class GroundCentre {
+  constructor() {
+    this.x = canvas.width
+    this.y = canvas.height - 120
+    this.type = groundCentreImage
+  }
+
+  move() {
+    this.x -= 5
+  }
+}
+var groundArray = [new GroundCentre()]
+
+
 
 class Dino {
   constructor() {
     this.x = 100
     this.y = 100
+    this.jumpCounter = 0
+    this.spaceCounter = 0
   }
 
   applyGravity() {
-    this.y += 5
+    this.y += 10
+  }
+
+  applyJump() {
+    if (this.jumpCounter > 0) {
+      this.y -= (Math.min(this.jumpCounter, 30) / 5) * 5
+      this.jumpCounter -= 1
+    }
+  }
+
+  resetJump() {
+    this.spaceCounter = 0
+  }
+
+  jump() {
+    if (this.spaceCounter < 1) {
+      this.spaceCounter += 1
+      this.jumpCounter = 30
+    }
   }
 }
 var dino = new Dino()
+
+document.body.onkeyup = function(e){
+  if(e.keyCode == 32){
+    dino.jump()
+  }
+}
+
 
 
 class Block {
@@ -59,6 +108,7 @@ blocks.push(new Block())
 
 
 
+
 function draw() {
   // Draw background
   for (var i = 0; i < backgroundArray.length; i++) {
@@ -71,10 +121,39 @@ function draw() {
     }
   }
 
+  // Draw Ground
+  for (var i = 0; i < groundArray.length; i++) {
+    canvasContext.drawImage(groundArray[i].type, groundArray[i].x, groundArray[i].y, 120, 120)
+    if ((i == groundArray.length - 1) && (groundArray[i].x <= canvas.width - 120)) {
+      groundArray.push(new GroundCentre())
+      
+    }
+    if (groundArray[i].x <= -240) {
+      groundArray.splice(i, 1);
+    }
+    else {
+      groundArray[i].move()
+    }
+  }
+
+
 
   // Draw Dino
-  canvasContext.drawImage(dinoImage,dino.x,dino.y,100,100)
-  dino.applyGravity()
+  canvasContext.drawImage(dinoImage,dino.x,dino.y,120,120)
+  if (groundArray[0].x <= 500) {
+    let filteredGround = groundArray.filter(function(item) {
+        return item.x >= -20 && item.x <= 220
+    })
+    if (filteredGround.length > 0 && (dino.y <= canvas.height - 240 && dino.y >= canvas.height - 259)) {
+      dino.resetJump()
+      dino.y = canvas.height - 240
+    }
+    else {
+      dino.applyGravity()
+    }
+    dino.applyJump()
+  }
+  
 
 
 
@@ -84,7 +163,7 @@ function draw() {
     blocks[i].move()
 
     if ((i == blocks.length - 1) && (blocks[i].x <= canvas.width - 240)) {
-      if (Math.random(1) < 0.05) {
+      if (Math.random(1) < 0.01) {
         blocks.push(new Block())
       }
     }
