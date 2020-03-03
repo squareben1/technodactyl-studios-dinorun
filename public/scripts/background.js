08896dc6ce62167962926cf444b24b24c7bf476c
Squareben1 <bengittins@live.com>
2020-03-03 11:23:49 +0000

    added x, y, canvas attributes to background, added reset function

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,11 +1,16 @@
 
 class Background {
-  constructor() {
+  constructor(canvas) {
     this.x = 0
-
+    this.y = 0
+    this.canvas = canvas
   }
 
   move() {
     this.x -= 2.5
+  }
+
+  reset() {
+    this.x = this.canvas.width
   }
 }
--- spec/backgroundSpec.js
+++ spec/backgroundSpec.js
@@ -1,8 +1,25 @@
 
 describe("Background", function() {
+  canvasDouble = {
+    width: 1280
+  }
+
+  it("should have y attribute of 0", function() {
+    background = new Background(canvasDouble)
+    expect(background.y).toEqual(0)
+  })
+
   it("move should deduct 2.5 from x axis", function() {
-    background = new Background
+    background = new Background(canvasDouble)
     background.move()
     expect(background.x).toEqual(-2.5)
   })
+
+  it("reset should reset x axis to canvas.width", function() {
+    background = new Background(canvasDouble)
+    background.move()
+    background.reset()
+    expect(background.x).toEqual(canvasDouble.width)
+  })
+  
 })
