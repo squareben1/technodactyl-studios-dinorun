5db396d94f4a73be419555ebfcb49e662c723ed8
Squareben1 <bengittins@live.com>
2020-03-03 12:06:11 +0000

    renderGame made

--- public/scripts/renderGame.js
+++ public/scripts/renderGame.js
@@ -1,19 +1,26 @@
 class RenderGame {
-  renderGame() {
-    var canvas = document.getElementById('canvas')
-    var canvasContext = canvas.getContext('2d')
-    // Create image objcects
-    var dinoImage = new Image()
-    dinoImage.src = 'images/dino_run.png'
-    var backgroundImage = new Image()
-    backgroundImage.src = 'images/background_desert.png'
-    var stoneBlock = new Image()
-    stoneBlock.src = 'images/stone_block.png'
-    var groundCentreImage = new Image()
-    var groundLeftImage = new Image()
-    var groundRightImage = new Image()
-    groundCentreImage.src = 'images/ground_centre.png'
-    groundLeftImage.src = 'images/ground_left.png'
-    groundRightImage.src = 'images/ground_right.png'
+  setup() {
+    this.canvas = document.getElementById('canvas')
+    this.canvasContext = canvas.getContext('2d')
+    this._generateImages()
+    this.backgroundArray = []
+    this.groundArray = []
+    this.blockArray = []
+  }
+
+  _generateImages() {
+    // Create image objects
+    this.dinoImage = new Image()
+    dinoImage.src = 'images/dino_png/Run (2).png'
+    this.backgroundImage = new Image()
+    backgroundImage.src = 'images/deserttileset/png/BG.png'
+    this.stoneBlock = new Image()
+    stoneBlock.src = 'images/deserttileset/png/Objects/StoneBlock.png'
+    this.groundCentreImage = new Image()
+    groundCentreImage.src = 'images/deserttileset/png/2.png'
+    this.groundLeftImage = new Image()
+    groundLeftImage.src = 'images/deserttileset/png/1.png'
+    this.groundRightImage = new Image()
+    groundRightImage.src = 'images/deserttileset/png/3.png'
   }
 }
