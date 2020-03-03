49e5b9851945d8353bb6721e7054432672389206
Squareben1 <bengittins@live.com>
2020-03-03 16:12:11 +0000

    added applyGravity and jump to dino

--- app/javascript/packs/dino.js
+++ app/javascript/packs/dino.js
@@ -2,9 +2,18 @@
 class Dino {
   constructor() {
     this.y = 0
+    this.spaceCounter = 0
+    this.jumpCounter = 0
   }
 
   applyGravity() {
     this.y += 10
+  }
+
+  jump() {
+    if (this.spaceCounter < 1) {
+      this.spaceCounter += 1
+      this.jumpCounter = 30
+    }
   }
 }
--- spec/js/dinoSpec.js
+++ spec/js/dinoSpec.js
@@ -1,8 +1,22 @@
 
 describe("Dino", function() {
-  it("applyGravity adds 10 to y axis", function() {
+
+  beforeAll(function () {
     dino = new Dino
+  })
+
+  it("applyGravity adds 10 to y axis", function() {
     dino.applyGravity()
     expect(dino.y).toEqual(10)
   })
+
+  it("jump increases spacecounter by 1 & jumpCounter by 30", function() {
+    dino.jump()
+    expect(dino.jumpCounter).toEqual(30)
+    expect(dino.spaceCounter).toEqual(1)
+  })
+
+  // it("resetJump resets spaceCounter", function() { // rename?
+
+  // })
 })
