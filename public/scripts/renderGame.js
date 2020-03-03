4e99315a030d1a4407da189d779036366bf616cd
Squareben1 <bengittins@live.com>
2020-03-03 12:16:29 +0000

    pushed backgrounds into array in Rendergame, created _drawNewGame

--- public/scripts/renderGame.js
+++ public/scripts/renderGame.js
@@ -1,26 +1,40 @@
 class RenderGame {
+  constructor(canvas) {
+    this.canvas = canvas
+    this.canvasContext = this.canvas.getContext('2d')
+  }
+
   setup() {
-    this.canvas = document.getElementById('canvas')
-    this.canvasContext = canvas.getContext('2d')
     this._generateImages()
     this.backgroundArray = []
     this.groundArray = []
     this.blockArray = []
+    this._drawNewGame()
+  }
+
+  _drawNewGame() {
+    this.backgroundArray = []
+    this.backgroundArray.push(new Background(this.canvas, this.backgroundImage))
+    var secondBackground = new Background(this.canvas, this.backgroundImage)
+    secondBackground.reset()
+    this.backgroundArray.push(secondBackground)
+    // for (var i = 0; i < this.canvas.clientWidth; i++)
+    //   this.backgroundArray.push()
   }
 
   _generateImages() {
     // Create image objects
     this.dinoImage = new Image()
-    dinoImage.src = 'images/dino_png/Run (2).png'
+    this.dinoImage.src = 'images/dino_png/Run (2).png'
     this.backgroundImage = new Image()
-    backgroundImage.src = 'images/deserttileset/png/BG.png'
+    this.backgroundImage.src = 'images/deserttileset/png/BG.png'
     this.stoneBlock = new Image()
-    stoneBlock.src = 'images/deserttileset/png/Objects/StoneBlock.png'
+    this.stoneBlock.src = 'images/deserttileset/png/Objects/StoneBlock.png'
     this.groundCentreImage = new Image()
-    groundCentreImage.src = 'images/deserttileset/png/2.png'
+    this.groundCentreImage.src = 'images/deserttileset/png/2.png'
     this.groundLeftImage = new Image()
-    groundLeftImage.src = 'images/deserttileset/png/1.png'
+    this.groundLeftImage.src = 'images/deserttileset/png/1.png'
     this.groundRightImage = new Image()
-    groundRightImage.src = 'images/deserttileset/png/3.png'
+    this.groundRightImage.src = 'images/deserttileset/png/3.png'
   }
 }
--- spec/renderGameSpec.js
+++ spec/renderGameSpec.js
@@ -1,4 +1,17 @@
 
 describe("RenderGame", function() {
-  
+
+  canvasDouble = {
+    width: 1280,
+    getContext: function(dimensions) {
+      return ''
+    }
+  }
+  imageDouble = {}
+
+  it("drawNewGame", function() {
+    renderGame = new RenderGame(canvasDouble)
+    renderGame.setup()
+    expect(renderGame.backgroundArray.length).toEqual(2)
+  })
 })
