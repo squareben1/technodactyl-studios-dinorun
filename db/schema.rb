fa38129cb080fd0cf0b57c05b2e655d5e7d2c703
cpcwood <cpcwood@outlook.com>
2020-03-04 11:26:21 +0000

    Remove password from user database, since bcrpyt uses password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_111246) do
+ActiveRecord::Schema.define(version: 2020_03_04_112353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2020_03_04_111246) do
   create_table "users", force: :cascade do |t|
     t.text "username"
     t.text "email"
-    t.text "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
