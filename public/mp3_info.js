e84db10cc3325c7bcdd0a7d5089b19229ddd5f54
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-03-04 13:46:56 +0000

    - hide title field - hide analysed field - hide tempo field - hide all these in _form.html.erb

--- public/mp3_info.js
+++ public/mp3_info.js
@@ -2,12 +2,13 @@ var audio_file = document.getElementById("song_mp3");
 
 audio_file.onchange = function() {
   var file = this.files[0];
+  song_title.value = file.name;
   var reader = new FileReader();
   var context = new (window.AudioContext || window.webkitAudioContext)();
   reader.onload = function() {
     context.decodeAudioData(reader.result, function(buffer) {
       json(buffer);
-      bpm(buffer)
+      bpm(buffer);
     });
   };
   reader.readAsArrayBuffer(file);
@@ -27,42 +28,39 @@ function json(buffer) {
   filter.connect(offlineContext.destination);
   source.start(0);
   offlineContext.startRendering().then(function(lowPassAudioBuffer) {
- 
-    var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     var song = audioCtx.createBufferSource();
     song.buffer = lowPassAudioBuffer;
     song.connect(audioCtx.destination);
     window.lowPassBuffer = song.buffer.getChannelData(0);
-    
+
     lowPassBuffer = getSampleClip(lowPassBuffer, 150);
     lowPassBuffer = normalizeArray(lowPassBuffer);
-    lowPassBuffer = lowPassBuffer.filter(function (value) {
+    lowPassBuffer = lowPassBuffer.filter(function(value) {
       return !Number.isNaN(value);
-  });
+    });
 
     song_analysed.value = JSON.stringify(lowPassBuffer);
-   });
+  });
 }
 
 function getSampleClip(data, samples) {
-
   var newArray = [];
   var modulus_coefficient = Math.round(data.length / samples);
 
   for (var i = 0; i < data.length; i++) {
-     if (i % modulus_coefficient == 0) {
-         newArray.push(data[i]);
-     }
+    if (i % modulus_coefficient == 0) {
+      newArray.push(data[i]);
+    }
   }
   return newArray;
 }
 
 function normalizeArray(data) {
-
   var newArray = [];
 
   for (var i = 0; i < data.length; i++) {
-      newArray.push(Math.abs(Math.round((data[i + 1] - data[i]) * 1000)));
+    newArray.push(Math.abs(Math.round((data[i + 1] - data[i]) * 1000)));
   }
 
   return newArray;
@@ -84,8 +82,7 @@ function bpm(buffer) {
   filter.connect(offlineContext.destination);
   source.start(0);
   offlineContext.startRendering().then(function(lowPassAudioBuffer) {
- 
-    var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     var song = audioCtx.createBufferSource();
     song.buffer = lowPassAudioBuffer;
     song.connect(audioCtx.destination);
@@ -97,66 +94,68 @@ function bpm(buffer) {
     lowPassBuffer = getSampleClip(lowPassBuffer, 400);
     lowPassBuffer = normalizeArray(lowPassBuffer);
     var tempo = countFlatLineGroupings(lowPassBuffer);
-    console.log(tempo)
     song_bpm.value = tempo * 6;
-   });
+  });
 }
 
 function getClip(length, startTime, data) {
-
   var clip_length = length * 44100;
   var section = startTime * 44100;
   var newArr = [];
 
   for (var i = 0; i < clip_length; i++) {
-     newArr.push(data[section + i]);
+    newArr.push(data[section + i]);
   }
 
   return newArr;
 }
 
 function countFlatLineGroupings(data) {
-
   var groupings = 0;
   var newArray = normalizeArray(data);
- 
+
   function getMax(a) {
-     var m = -Infinity,
-         i = 0,
-         n = a.length;
- 
-     for (; i != n; ++i) {
-         if (a[i] > m) {
-             m = a[i];
-         }
-     }
-     return m;
+    var m = -Infinity,
+      i = 0,
+      n = a.length;
+
+    for (; i != n; ++i) {
+      if (a[i] > m) {
+        m = a[i];
+      }
+    }
+    return m;
   }
- 
+
   function getMin(a) {
-     var m = Infinity,
-         i = 0,
-         n = a.length;
- 
-     for (; i != n; ++i) {
-         if (a[i] < m) {
-             m = a[i];
-         }
-     }
-     return m;
+    var m = Infinity,
+      i = 0,
+      n = a.length;
+
+    for (; i != n; ++i) {
+      if (a[i] < m) {
+        m = a[i];
+      }
+    }
+    return m;
   }
- 
+
   var max = getMax(newArray);
   var min = getMin(newArray);
   var count = 0;
   var threshold = Math.round((max - min) * 0.2);
- 
+
   for (var i = 0; i < newArray.length; i++) {
- 
-    if (newArray[i] > threshold && newArray[i + 1] < threshold && newArray[i + 2] < threshold && newArray[i + 3] < threshold && newArray[i + 6] < threshold) {
-         count++;
-     }
+    if (
+      newArray[i] > threshold &&
+      newArray[i + 1] < threshold &&
+      newArray[i + 2] < threshold &&
+      newArray[i + 3] < threshold &&
+      newArray[i + 6] < threshold
+    ) {
+      count++;
+    }
   }
- 
+
   return count;
- }
+}
