240ed12ff9e34fc07f297fc7fed299502d5ee8ee
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-03-05 18:02:05 +0000

    returns second image after 30 fps

--- app/javascript/packs/dino.js
+++ app/javascript/packs/dino.js
@@ -1,42 +1,48 @@
-
 class Dino {
   constructor(imageArray) {
-    this.x = 100
-    this.y = 100
-    this.xSize = 120
-    this.ySize = 120
-    this.spaceCounter = 0
-    this.jumpCounter = 0
-    this.imageArray = imageArray
-    this.animationCounter = 0
+    this.x = 100;
+    this.y = 100;
+    this.xSize = 120;
+    this.ySize = 120;
+    this.spaceCounter = 0;
+    this.jumpCounter = 0;
+    this.imageArray = imageArray;
+    this.animationCounter = 0;
   }
 
   applyGravity() {
-    this.y += 10
+    this.y += 10;
   }
 
   applyJump() {
     if (this.jumpCounter > 0) {
-      this.y -= (this.jumpCounter / 5) * 5
-      this.jumpCounter -= 1
+      this.y -= (this.jumpCounter / 5) * 5;
+      this.jumpCounter -= 1;
     }
   }
 
   resetJump() {
-    this.spaceCounter = 0
+    this.spaceCounter = 0;
   }
 
   jump() {
     if (this.spaceCounter < 1) {
-      this.spaceCounter += 1
-      this.jumpCounter = 30
+      this.spaceCounter += 1;
+      this.jumpCounter = 30;
     }
   }
 
   image() {
-    return this.imageArray[0]
+    if (this.animationCounter < 30) {
+      return this.imageArray[0];
+    } else if (this.animationCounter < 30 * 2) {
+      return this.imageArray[1];
+    } else if (this.animationCounter < 60 + 1) {
+      this.animationCounter = 0;
+      return this.imageArray[0];
+    }
+    m;
   }
-
 }
 
-window.Dino = Dino
+window.Dino = Dino;
--- spec/js/dinoSpec.js
+++ spec/js/dinoSpec.js
@@ -1,53 +1,52 @@
-
 describe("Dino", function() {
+  beforeEach(function() {
+    imageDouble = {};
+    imageDouble2 = { a: 1 };
+    dinoImageArray = [imageDouble, imageDouble2];
+    dino = new Dino(dinoImageArray);
+  });
 
-  beforeEach(function () {
-    imageDouble = {}
-    imageDouble2 = {}
-    dinoImageArray = [imageDouble, imageDouble2]
-    dino = new Dino(dinoImageArray)
-  })
-
-  
-  describe('#applyGravity', function() {
+  describe("#applyGravity", function() {
     it("applyGravity adds 10 to y axis", function() {
-      dino.applyGravity()
-      expect(dino.y).toEqual(110)
-    })
-  })
+      dino.applyGravity();
+      expect(dino.y).toEqual(110);
+    });
+  });
 
-
-  describe('#applyJump', function() {   
+  describe("#applyJump", function() {
     it("if spaceCounter > 0 -jumpCounter from Y -1 from jumpCounter", function() {
-      dino.jumpCounter = 10
-      dino.applyJump()
-      expect(dino.y).toEqual(90)
-      expect(dino.jumpCounter).toEqual(9)
-    })
-  })
+      dino.jumpCounter = 10;
+      dino.applyJump();
+      expect(dino.y).toEqual(90);
+      expect(dino.jumpCounter).toEqual(9);
+    });
+  });
 
-  describe('#resetJump', function() {   
-    it("resets spaceCounter after jump", function() { // rename?
-      dino.spaceCounter = 2
-      dino.resetJump()
-      expect(dino.spaceCounter).toEqual(0)
-    })
-  })
+  describe("#resetJump", function() {
+    it("resets spaceCounter after jump", function() {
+      // rename?
+      dino.spaceCounter = 2;
+      dino.resetJump();
+      expect(dino.spaceCounter).toEqual(0);
+    });
+  });
 
-
-  describe('#jump', function() {   
+  describe("#jump", function() {
     it("jump increases spacecounter by 1 & sets jumpCounter to 30", function() {
-      dino.jump()
-      expect(dino.jumpCounter).toEqual(30)
-      expect(dino.spaceCounter).toEqual(1)
-    })
-  })
+      dino.jump();
+      expect(dino.jumpCounter).toEqual(30);
+      expect(dino.spaceCounter).toEqual(1);
+    });
+  });
 
-
-  describe('#image', function() {
+  describe("#image", function() {
     it("returns first image in dinoImgArray", function() {
-      console.log(dino.imageArray[0])
-      expect(dino.image()).toEqual(dino.imageArray[0])
-    })
-  })
-})
+      expect(dino.image()).toEqual(dino.imageArray[0]);
+    });
+
+    it("returns second image in dinoImgArray", function() {
+      dino.animationCounter = 31;
+      expect(dino.image()).toEqual(dino.imageArray[1]);
+    });
+  });
+});
