35f742d0809db43823162ef289faa24986289dad
cpcwood <cpcwood@outlook.com>
2020-03-09 11:35:33 +0000

    production fix

--- app/javascript/packs/jasmine.js
+++ app/javascript/packs/jasmine.js
@@ -1,4 +1,4 @@
-import 'jasmine-core/lib/jasmine-core/jasmine.css'
-import 'jasmine-core/lib/jasmine-core/jasmine-html.js'
-import 'jasmine-core/lib/jasmine-core/boot.js'
-import 'jasmine-core/images/jasmine_favicon.png'
+import '../../../node_modules/jasmine-core/lib/jasmine-core/jasmine.css'
+import '../../../node_modules/jasmine-core/lib/jasmine-core/jasmine-html.js'
+import '../../../node_modules/jasmine-core/lib/jasmine-core/boot.js'
+import '../../../node_modules/jasmine-core/images/jasmine_favicon.png'
