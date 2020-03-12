bd1686fa7fbcb3b28e7578f05e5b3b2183ea8407
Umberto Valente <uvalente89@gmail.com>
2020-03-12 23:01:03 +0000

    hide #user-message when showing instruction

--- app/javascript/packs/homepage_interface/userInterface.js
+++ app/javascript/packs/homepage_interface/userInterface.js
@@ -8,6 +8,7 @@ function toggleSignUpForm() {
 
 function toggleInstructions(){
   $("#instruction").toggle()
+  $("#user-message").hide()
 }
 
 export { toggleLogInForm, toggleSignUpForm, toggleInstructions } 
